ba3212127d0be0f25be3743209fb78ccad718f50
Anna.Krajuskina <anna.krajuskina@gmail.com>
2018-05-25 14:14:26 +0300

    Add estonian lexical attributes

---
+++ spacy/lang/et/lex_attrs.py
@@ -0,0 +1,29 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+# import the symbols for the attrs you want to overwrite
+from ...attrs import LIKE_NUM
+
+_num_words = ['null', 'üks', 'kaks', 'kolm', 'neli', 'viis', 'kuus', 'seitse', 'kaheksa', 'üheksa', 'kümme',
+              'üksteist', 'kaksteist', 'kolmteist', 'neliteist', 'viisteist', 'kuusteist', 'seitseteist',
+              'kaheksateist', 'üheksateist', 'kakskümmend', 'kolmkümmend', 'nelikümmend', 'viiskümmend',
+              'kuuskümmend', 'seitsekümmend', 'kaheksakümmend', 'üheksakümmend',
+              'sada', 'tuhat', 'miljon', 'miljard', 'triljon', 'kvadriljon']
+
+
+def like_num(text):
+    text = text.replace(',', '').replace('.', '')
+    if text.isdigit():
+        return True
+    if text.count('/') == 1:
+        num, denom = text.split('/')
+        if num.isdigit() and denom.isdigit():
+            return True
+    if text in _num_words:
+        return True
+    return False
+
+
+LEX_ATTRS = {
+    LIKE_NUM: like_num
+}
