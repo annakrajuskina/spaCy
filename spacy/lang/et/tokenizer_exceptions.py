9de1191d8fbe5c42b504a3a843b08ca296c47f91
Anna.Krajuskina <anna.krajuskina@gmail.com>
2018-05-26 23:05:08 +0300

    Add more tokenizer exceptions

--- spacy/lang/et/tokenizer_exceptions.py
+++ spacy/lang/et/tokenizer_exceptions.py
@@ -16,6 +16,7 @@ _abbrev_exc = [
     {ORTH: "L", LEMMA: "laupäev", NORM: "laupäev"},
     {ORTH: "P", LEMMA: "pühapäev", NORM: "pühapäev"},
 
+    # http://www.eki.ee/itstandard/2000/FDCC.shtml
     # Months abbreviations
     {ORTH: "jaan", LEMMA: "jaanuar", NORM: "jaanuar"},
     {ORTH: "veebr", LEMMA: "veebruar", NORM: "veebruar"},
@@ -37,12 +38,43 @@ _abbrev_exc = [
     {ORTH: "nove", LEMMA: "november", NORM: "november"},
     {ORTH: "dets", LEMMA: "detsember", NORM: "detsember"},
     {ORTH: "detse", LEMMA: "detsember", NORM: "detsember"},
+
+    # http://www.eki.ee/dict/qs2006/lyhendid.html
+    {ORTH: "al.", LEMMA: "alates", NORM: "alates"},
+    {ORTH: "AS", LEMMA: "aktsiaselts", NORM: "aktsiaselts"},
+    {ORTH: "dir", LEMMA: "direktor", NORM: "direktor"},
+    {ORTH: "dr", LEMMA: "doktor", NORM: "doktor"},
+    {ORTH: "dr.", LEMMA: "doktor", NORM: "doktor"},
+    {ORTH: "e.m.a", LEMMA: "enne meie ajaarvamist", NORM: "enne meie ajaarvamist"},
+    {ORTH: "m.a.j", LEMMA: "meie ajaarvamise järgi", NORM: "meie ajaarvamise järgi"},
+    {ORTH: "FIE", LEMMA: "füüsilisest isikust ettevõtja", NORM: "füüsilisest isikust ettevõtja"},
+    {ORTH: "OÜ", LEMMA: "osaühing", NORM: "osaühing"},
+    {ORTH: "jm", LEMMA: "ja muu", NORM: "ja muu"},
+    {ORTH: "jms", LEMMA: "ja muud sellised", NORM: "ja muud sellised"},
+    {ORTH: "jmt", LEMMA: "ja mitmed teised", NORM: "ja mitmed teised"},
+    {ORTH: "jne", LEMMA: "ja nii edasi", NORM: "ja nii edasi"},
+    {ORTH: "jpm", LEMMA: "ja palju muud", NORM: "ja palju muud"},
+    {ORTH: "ms", LEMMA: "muu seas", NORM: "muu seas"},
+    {ORTH: "pms", LEMMA: "peamiselt", NORM: "peamiselt"},
+    {ORTH: "st", LEMMA: "see tähendab", NORM: "see tähendab"},
+    {ORTH: "s.t", LEMMA: "see tähendab", NORM: "see tähendab"},
+    {ORTH: "s.o", LEMMA: "see on", NORM: "see on"},
+    {ORTH: "vt", LEMMA: "vaata", NORM: "vaata"},
+    {ORTH: "u", LEMMA: "umbes", NORM: "umbes"},
+    {ORTH: "tn", LEMMA: "tänav", NORM: "tänav"},
+    {ORTH: "mnt", LEMMA: "maantee", NORM: "maantee"},
+    {ORTH: "pst", LEMMA: "puiestee", NORM: "puiestee"},
+    {ORTH: "ptk", LEMMA: "peatükk", NORM: "peatükk"},
+    {ORTH: "s.a", LEMMA: "sel aastal", NORM: "sel aastal"},
+    {ORTH: "saj", LEMMA: "sajand", NORM: "sajand"},
+
 ]
 
 for abbrev_desc in _abbrev_exc:
     abbrev = abbrev_desc[ORTH]
     for orth in (abbrev, abbrev.capitalize(), abbrev.upper()):
         _exc[orth] = [{ORTH: orth, LEMMA: abbrev_desc[LEMMA], NORM: abbrev_desc[NORM]}]
-        _exc[orth + '.'] = [{ORTH: orth + '.', LEMMA: abbrev_desc[LEMMA], NORM: abbrev_desc[NORM]}]
+        if not orth.endswith('.'):
+            _exc[orth + '.'] = [{ORTH: orth + '.', LEMMA: abbrev_desc[LEMMA], NORM: abbrev_desc[NORM]}]
 
 TOKENIZER_EXCEPTIONS = _exc
