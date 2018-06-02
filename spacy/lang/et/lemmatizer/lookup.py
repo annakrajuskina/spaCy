0a7eaf1cae8c9fca4d150ba1daeff8d838e72427
Anna.Krajuskina <anna.krajuskina@gmail.com>
2018-06-03 01:14:23 +0300

    Improve lemmatizer

--- spacy/lang/et/lemmatizer/lookup.py
+++ spacy/lang/et/lemmatizer/lookup.py
@@ -6,11 +6,12 @@ import os
 from collections import defaultdict
 
 # https://raw.githubusercontent.com/michmech/lemmatization-lists/master/lemmatization-et.txt
-fnm = os.path.join(os.path.curdir(), 'dict.bz2')
+fnm = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'dict.bz2')
+print(fnm)
 lemmas = defaultdict(list)
 with bz2.BZ2File(fnm) as inf:
     for ln in inf:
         infl, lem = ln.decode('utf8').rstrip().split("\t", 1)
-        lemmas[infl].append(lem[0])
+        lemmas[infl].append(lem)
 
-LOOKUP = lemmas
+LOOKUP = {}
